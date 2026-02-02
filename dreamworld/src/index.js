c21669176624140d536b40b58bea027e798da616
SHARVANIRAISETTI <sharvani2410@gmail.com>
2026-02-02 19:31:21 +0530

    Update project files

--- dreamworld/src/index.js
+++ dreamworld/src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-import './index.css';
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
