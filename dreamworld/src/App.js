8213b9e104f2e70386494dc54b29f726a5d6d790
SHARVANIRAISETTI <sharvani2410@gmail.com>
2026-02-02 20:56:03 +0530

    check

--- dreamworld/src/App.js
+++ dreamworld/src/App.js
@@ -1,33 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
-import { AuthProvider } from "./context/AuthContext";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { CartProvider } from "./context/CartContext";
+// import { AuthProvider } from "./context/AuthContext";
 
-import Header from "./components/Header";
-import ProtectedRoute from "./components/ProtectedRoute";
+// import Header from "./components/Header";
+// import ProtectedRoute from "./components/ProtectedRoute";
 
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Login from "./pages/Login";
-import Profile from "./pages/Profile";
+// import Home from "./pages/Home";
+// import Cart from "./pages/Cart";
+// import Checkout from "./pages/Checkout";
+// import Login from "./pages/Login";
+// import Profile from "./pages/Profile";
 
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <AuthProvider>
+//         <CartProvider>
+//           <Header />
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/login" element={<Login />} />
+//             <Route path="/profile" element={<Profile />} />
+//             <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+//             <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
+//           </Routes>
+//         </CartProvider>
+//       </AuthProvider>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
 function App() {
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
-            <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
-          </Routes>
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  );
+  return <h1>DreamWorld Live 🚀</h1>;
 }
-
 export default App;
+
